2e08453a628cd540ae41be11d2b3080288fccb1e
Dmitry Redkin <dima.redkin.1999@gmail.com>
2022-10-15 17:31:42 +0500

    feature: Added yaml config.
    Use yaml config in main cmd.

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,39 @@
 package main
 
 import (
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"log"
+
 	"go-crdt-load-test/loader"
 	"go-crdt-load-test/report"
 	"go-crdt-load-test/schedule"
-	"log"
 )
 
 func main() {
-	// Call 9 Increments and 1 Count, 10 times.
-	// 9*10 = 90 is a total Increments counts.
-	// 1*10 = 10 is a total Count calls count.
-	// (9+1)*10 = 100 is a total requests count.
-	loaderConfig := loader.Config{
-		CountsCount:      10,
-		IncsPerCountCall: 9,
+	yamlFile, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		log.Fatal(err)
 	}
+	var loaderConfig loader.Config
+	err = yaml.Unmarshal(yamlFile, &loaderConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO move addresses to config
 	rr := schedule.NewRoundRobin([]string{
 		"http://localhost:8000",
 		"http://localhost:8001",
 		"http://localhost:8002",
 	})
+
 	l := loader.NewLoader(loaderConfig, rr)
 	rep, err := l.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	err = report.WriteToFile(rep, "report.txt")
 	if err != nil {
 		log.Fatal(err)
--- loader/config.go
+++ loader/config.go
@@ -1,6 +1,11 @@
 package loader
 
+// Config for Loader.
+// Call IncsPerCountCall Increments and 1 Count, CountsCount times.
+// IncsPerCountCall*CountsCount is a total Increments counts.
+// CountsCount is a total Count calls count.
+// (IncsPerCountCall+1)*CountsCount is a total requests count.
 type Config struct {
-	CountsCount      int
-	IncsPerCountCall int
+	CountsCount      int `yaml:"counts_count"`
+	IncsPerCountCall int `yaml:"incs_per_count_call"`
 }
